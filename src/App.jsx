7063e9a3d7867b1b2a303aa1c99a493dcd69c8f7
pulze <malagodap2@gmail.com>
2024-12-10 10:18:33 +0530

    Result component added.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header"
+import Results from "./components/Results"
 import UserInput from "./components/UserInput"
 
 function App() {
   return (
     <>
-    <Header/>
-    <UserInput/>
+      <Header/>
+      <UserInput/>
+      <Results/>
     </>
   )
 }
--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -22,21 +22,49 @@ export default function UserInput() {
             <div className="input-group">
                 <p>
                     <label>Initial Investment</label>
-                    <input type="number" required />
+                    <input 
+                        type="number" 
+                        required
+                        value={userInput.initialInvestment}
+                        onChange={(event) => 
+                            handleChange('initialInvestment', event.target.value)
+                        } 
+                    />
                 </p>
                 <p>
                     <label>Annual Investment</label>
-                    <input type="number" required />
+                    <input 
+                        type="number" 
+                        required 
+                        value={userInput.annualInvestment}
+                        onChange={(event) => 
+                            handleChange('annualInvestment', event.target.value)
+                        }
+                    />
                 </p>
             </div>
             <div className="input-group">
                 <p>
                     <label>Expected Return</label>
-                    <input type="number" required />
+                    <input 
+                        type="number" 
+                        required
+                        value={userInput.expectedReturn}
+                        onChange={(event) => 
+                            handleChange('expectedReturn', event.target.value)
+                        }
+                    />
                 </p>
                 <p>
                     <label>Duration</label>
-                    <input type="number" required />
+                    <input 
+                        type="number" 
+                        required 
+                        value={userInput.duration}
+                        onChange={(event) => 
+                            handleChange('duration', event.target.value)
+                        }
+                    />
                 </p>
             </div>
         </section>
